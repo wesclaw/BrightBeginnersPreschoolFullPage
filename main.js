b611ffd8d9a18437c2febea6d59e3d89bfc563cb
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-05-15 20:03:13 +0200

    Loading bar
    Added a loading bar to the page

--- main.js
+++ main.js
@@ -3,17 +3,35 @@ const date = new Date();
 let y = date.getFullYear();
 year_tag.textContent = y;
 
-AOS.init();
 
 
-const navbar = document.querySelector('.navbar')
-let lastScrollY = window.scrollY;
 
-window.addEventListener('scroll',()=>{
-  if(lastScrollY < window.scrollY){
-    navbar.classList.add('nav-hidden')
-  }else{
-    navbar.classList.remove('nav-hidden')
-  }
-  lastScrollY = window.scrollY
+
+
+
+
+window.addEventListener('load',()=>{
+  const loader = document.querySelector('.loader')
+
+  loader.classList.add('loader-hidden')
+
+  AOS.init();
+  const navbar = document.querySelector('.navbar')
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener('scroll',()=>{
+    if(lastScrollY < window.scrollY){
+      navbar.classList.add('nav-hidden')
+    }else{
+      navbar.classList.remove('nav-hidden')
+    }
+    lastScrollY = window.scrollY
+  })
+  
+
+  loader.addEventListener('transitionend',()=>{
+    document.body.removeChild('loader')
+  })
+
+
 })
